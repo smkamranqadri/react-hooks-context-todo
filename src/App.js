1f78075e0f6b891be8f3e385f4a96c4fa6afc1dc
Muhammad Kamran <smkamranqadri@yahoo.com>
2020-01-17 12:11:13 +0500

    todo app creted using hooks.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Div, H1, H2, Input, Button } from 'styled-system-html';
 
 function App() {
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2']);
+  const [todo, setTodo] = useState('');
+  const [selectTodoIndex, setSelectedTodoIndex] = useState(null);
+  const addTodo = () => {
+    if (selectTodoIndex !== null) {
+      todos[selectTodoIndex] = todo;
+      setTodos([...todos]);
+    } else {
+      setTodos([...todos, todo]);
+      setTodo('');
+    }
+  };
+  const editTodo = (todo, index) => {
+    setSelectedTodoIndex(index);
+    setTodo(todo);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div width="762px" margin="auto">
+      <Div display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+        <H1>Todo List!</H1>
+        <Div display="flex" justifyContent="center">
+          <Input type="text" bg="yellow" padding="10px" width="300px" border="solid 1px black" value={todo} onChange={e => setTodo(e.target.value)} />
+          <Button padding="10px" onClick={() => addTodo()}>
+            {selectTodoIndex !== null ? 'Save' : 'Add'}
+          </Button>
+        </Div>
+        <Div width="100%" display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+          {todos.map((todo, index) => (
+            <Div width="100%" display="flex" justifyContent="space-between" marginTop="10px" key={index}>
+              <H2>{todo}</H2>
+              <Div>
+                <Button padding="10px" marginRight="10px" onClick={() => editTodo(todo, index)}>
+                  Edit
+                </Button>
+                <Button bg="red" color="white" padding="10px" onClick={() => setTodos(todos.filter(item => item !== todo))}>
+                  Delete
+                </Button>
+              </Div>
+            </Div>
+          ))}
+        </Div>
+      </Div>
+    </Div>
   );
 }
 
